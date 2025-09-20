19db10d30634541b87afe6b22ce6ec19c23a8d04
luisoi1993 <luisaguadodiego1993@gmail.com>
2025-09-20 23:58:25 +0200

    clase 2

---
+++ t1/Tienda/src/Main.kt
@@ -0,0 +1,31 @@
+import model.Producto
+
+
+
+fun main() {
+
+    var camiseta : Producto = Producto(id = 1, precio = 14.99)
+    var zapatillas = Producto(2,20.0)
+    // id , precio, nombre
+    var pantalones = Producto(3,30.0,"Pantalones")
+    var gorra = Producto(4,20.0, descripcion = "Gorra molona")
+    var cartera = Producto(5,5.0,"Cartera","Cartera para guardar la pasta")
+
+    val listaProductos : Array<Producto?> = arrayOf(camiseta, zapatillas, pantalones, gorra, cartera)
+
+    cartera.mostrarDatos()
+    gorra.mostrarDatos()
+   // listaProductos[0].mostrarDatos()
+    //listaProductos[listaProductos.size-1].mostrarDatos()
+    listaProductos.first()
+    listaProductos.last()
+
+    listaProductos.last()?.precio = 20.0
+    listaProductos.last()?.mostrarDatos()
+    listaProductos[4] = null
+
+    for (i in listaProductos){
+        i?.mostrarDatos()
+    }
+
+}
---
+++ t1/Tienda/src/model/Producto.kt
@@ -0,0 +1,38 @@
+package model
+
+class Producto(var id: Int, var precio : Double = 10.0 , var nombre: String? = null , var descripcion: String? = null) {
+
+    // id
+    // precio
+    /*
+    var nombre: String? = null
+    var descripcion : String? = null
+
+    constructor(id: Int, precio: Double, nombre : String) : this(id,precio){
+        this.nombre = nombre
+    }
+    // crear constructor solo con nombre , solo con descripcion
+    // y otro con nombre y descripcion
+    constructor(id: Int, precio: Double, descripcion: String?) : this(id,precio){
+        this.descripcion = descripcion
+    }
+
+    constructor(id: Int, precio: Double, nombre: String, descripcion :String) : this(id,precio){
+        this.nombre = nombre
+        this.descripcion = descripcion
+    }
+    */
+     //Metodo para poder mostrara todos los datos del producto
+    // si no hay nombre aperce sin nombre
+    // si no hay descripcion aparece sin descripcion
+
+
+
+    fun mostrarDatos(){
+        println("ID: $id")
+        println("Precio: $precio")
+        println("Nombre ${nombre ?: "Sin definir"}")
+        println("Descripcion ${descripcion ?: "SIN DEFINIR"}")
+    }
+
+}
