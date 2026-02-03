810ab3b61db3b830d55c3171e7763761a072302f
luisoi1993 <luisaguadodiego1993@gmail.com>
2026-02-03 05:48:50 +0100

    trivial 1

---
+++ t6/TrivialFragments/app/src/main/java/com/example/trivialfragments/ui/fragment/LoginFragment.kt
@@ -0,0 +1,25 @@
+package com.example.trivialfragments.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.trivialfragments.databinding.FragmentLoginBinding
+
+class LoginFragment: Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+
+}
